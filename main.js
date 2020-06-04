17602b1aa74f6615fbaf0b7025e61763877908a9
hbpark2 <hbpark2@naver.com>
2020-06-04 21:09:05 +0900

    for mobile

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-    
+    var windowWidth = $(window).width()
     var windowHeight = $(window).height()
     console.log(windowHeight)
-    $(".main_con").css({marginTop:windowHeight-300})
-    
+    if(windowWidth>640){
+        $(".main_con").css({marginTop:windowHeight-300})
+    }
 })
 
